3a10900d46f4cbb8fc7ae4e2f870258fb7e26642
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-25 23:59:26 -0600

    implement method wikimediaStreamTopic for the project consumer_s3

--- src/main/java/com/consumer_s3/config/WikimediaTopicConfig.java
+++ src/main/java/com/consumer_s3/config/WikimediaTopicConfig.java
@@ -1,4 +1,17 @@
 package com.consumer_s3.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class WikimediaTopicConfig {
+
+  @Bean
+  public NewTopic wikimediaStreamTopic() {
+    return TopicBuilder
+      .name("wikimedia-stream")
+      .build();
+  }
 }
