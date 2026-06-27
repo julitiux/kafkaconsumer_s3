5eecc9497600fd943e2da8c67390f3d70bc30334
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-26 19:47:37 -0600

    implement method consumerMessage into the class WikimediaConsumer

--- src/main/java/com/consumer_s3/consumer/WikimediaConsumer.java
+++ src/main/java/com/consumer_s3/consumer/WikimediaConsumer.java
@@ -1,4 +1,16 @@
 package com.consumer_s3.consumer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class WikimediaConsumer {
+
+  @KafkaListener(topics = "wikimedia-stream", groupId = "myGroup")
+  public void consumerMessage(String message) {
+
+    log.info(String.format("Wikimedia consumer receive message: %s", message));
+  }
 }
