fc4a780db8f787465046be226d7a955c7f5bd15a
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-26 19:48:23 -0600

    rebuilt the imports, separate the static String.format

--- src/main/java/com/consumer_s3/consumer/WikimediaConsumer.java
+++ src/main/java/com/consumer_s3/consumer/WikimediaConsumer.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import static java.lang.String.format;
+
 @Slf4j
 @Service
 public class WikimediaConsumer {
@@ -11,6 +13,6 @@ public class WikimediaConsumer {
   @KafkaListener(topics = "wikimedia-stream", groupId = "myGroup")
   public void consumerMessage(String message) {
 
-    log.info(String.format("Wikimedia consumer receive message: %s", message));
+    log.info(format("Wikimedia consumer receive message: %s", message));
   }
 }
