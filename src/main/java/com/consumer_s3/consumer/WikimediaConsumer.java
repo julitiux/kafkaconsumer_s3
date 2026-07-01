eacc138f0a7d8659e5ede6178f641d7a79d48ae7
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-30 22:59:40 -0600

    inject bean of service S3Service and complement method consumerMessage

--- src/main/java/com/consumer_s3/consumer/WikimediaConsumer.java
+++ src/main/java/com/consumer_s3/consumer/WikimediaConsumer.java
@@ -1,5 +1,6 @@
 package com.consumer_s3.consumer;
 
+import com.consumer_s3.service.S3Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,22 @@ import static java.lang.String.format;
 @Service
 public class WikimediaConsumer {
 
+  private final S3Service s3Service;
+
+  public WikimediaConsumer(S3Service s3Service) {
+    this.s3Service = s3Service;
+  }
+
   @KafkaListener(topics = "wikimedia-stream", groupId = "myGroup")
   public void consumerMessage(String message) {
 
     log.info(format("Wikimedia consumer receive message: %s", message));
+
+    String filename =
+      "events/" +
+        System.currentTimeMillis() +
+        ".json";
+
+    s3Service.updload(filename, message);
   }
 }
