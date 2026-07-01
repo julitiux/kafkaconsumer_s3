27eb2c36b013e8add18335bf74e92da69fd63d15
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-30 22:56:41 -0600

    add implement on method updload S3Service

--- src/main/java/com/consumer_s3/service/S3Service.java
+++ src/main/java/com/consumer_s3/service/S3Service.java
@@ -1,4 +1,27 @@
 package com.consumer_s3.service;
 
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
 public class S3Service {
+
+  private final S3Client s3Client;
+
+  public S3Service(S3Client s3Client) {
+    this.s3Client = s3Client;
+  }
+
+  public void updload(String key, String content) {
+
+    PutObjectRequest request =
+      PutObjectRequest.builder()
+        .bucket("bucket3")
+        .key(key)
+        .build();
+
+    s3Client.putObject(request, RequestBody.fromString(content));
+  }
 }
